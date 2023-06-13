e95b58f83b6e1a87bd806e4edf75aecc743c9faa
Alexandre Wotawa <awotawa@gmail.com>
2023-06-13 15:09:48 +0200

    feat: create a car resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { CarModule } from './car/car.module';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -9,6 +10,6 @@ import { join } from 'path';
     playground: false, //remove this if you want to use the playground
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //will be creating a schema file
     sortSchema: true, //enables the sorting of the schema
-  }),],
+  }), CarModule,],
 })
 export class AppModule {}
