0e42b04b08d0bdc470192a0934a614607f6599ac
Alexandre Wotawa <awotawa@gmail.com>
2023-06-13 15:08:33 +0200

    feat: add imports to enable GraphQL use

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    playground: false, //remove this if you want to use the playground
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //will be creating a schema file
+    sortSchema: true, //enables the sorting of the schema
+  }),],
 })
 export class AppModule {}
