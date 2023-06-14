58d24434102a5442e697d8a8519006c6feea8929
Alexandre Wotawa <awotawa@gmail.com>
2023-06-14 15:56:44 +0200

    feat: enable prisma to be used in our service

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCarInput } from './dto/create-car.input';
 import { UpdateCarInput } from './dto/update-car.input';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CarService {
+  constructor(private prisma: PrismaService) {}
+
   create(createCarInput: CreateCarInput) {
     return 'This action adds a new car';
   }
