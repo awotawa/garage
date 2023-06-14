744f91b6be4183d34f9dcbfbf7edd4429d3b6b2b
Alexandre Wotawa <awotawa@gmail.com>
2023-06-14 15:52:27 +0200

    feat: create PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { CarModule } from './car/car.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -11,5 +12,6 @@ import { CarModule } from './car/car.module';
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //will be creating a schema file
     sortSchema: true, //enables the sorting of the schema
   }), CarModule,],
+  providers: [PrismaService],
 })
 export class AppModule {}
